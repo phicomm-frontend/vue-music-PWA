c8f7d21bb8f17c37f9977732f29b61545621e64d
zhihao.yu <zhihao.yu@phicomm.com>
2017-11-19 22:39:31 +0800

    add router

---
+++ src/components/tab/tab.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="my-tab">
+    <router-link tag="div" class="tab-item" :to="/recommend">
+      <span class="tab-link">推荐</span>
+    </router-link>
+    <router-link tag="div" class="tab-item" :to="/singer">
+      <span class="tab-link">歌手</span>
+    </router-link>
+    <router-link tag="div" class="tab-item" :to="/rank">
+      <span class="tab-link">排行
+      </span>
+    </router-link>
+    <router-link tag="div" class="tab-item" :to="/search">
+      <span class="tab-link">搜索</span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style scoped>
+  .my-tab {
+    display: flex;
+    height: 44px;
+    line-height: 44px;
+  }
+  .router-link-active {
+    color: orange;
+    border-bottom: 2px solid orange;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Rank from '@/components/rank/rank'
+import Recommend from '@/components/recommend/recommend'
+import Singer from '@/components/singer/singer'
+import Search from '@/components/search/search'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: '/rank',
+      component: Rank
+    },{
+      path: '/recommend',
+      component: Recommend
+    },{
+      path: '/singer',
+      component: Singer
+    },{
+      path: '/search',
+      component: Search
     }
   ]
 })
