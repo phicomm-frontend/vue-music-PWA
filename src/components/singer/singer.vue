4083702948aa142b0872b119e2b95db630df30b3
yuting.peng <yuting.peng@phicomm.com>
2017-11-27 19:35:36 +0800

    增加歌手推荐

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,17 +1,44 @@
 <template>
-  <div>
-    歌手页面
-  </div>
+  <div>歌手dd</div>
 </template>
 
 <script>
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  const HOT_NAME = '热门'
+
   export default {
-    data () {
-      return {}
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = res.data.list
+            console.log(this.singers)
+          } else {
+            console.log(2)
+          }
+        })
+      },
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+      }
     }
   }
 </script>
 
-<style scoped>
+<style>
 
 </style>
