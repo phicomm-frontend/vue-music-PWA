f6ed3fff35e8b41f19936d7f09efaab9c18f59e0
shaolun.ye <shaolun.ye@phicomm.com>
2017-12-11 16:12:14 +0800

    推荐页

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './common/scss/index.scss'
+import VueLazyload from 'vue-lazyload'
 Vue.config.productionTip = false
+Vue.use(VueLazyload, {
+  loading: require('common/image/wangzai.jpg')
+});
 
 /* eslint-disable no-new */
 new Vue({
